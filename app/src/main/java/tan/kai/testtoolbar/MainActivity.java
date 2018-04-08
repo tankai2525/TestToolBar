705b47125cb52a6628965577fc4d5b397f982ade
luobodidi <1067629251@qq.com>
2018-04-08 18:03:41 +0800

    2018年4月8日18:02:41

--- app/src/main/java/tan/kai/testtoolbar/MainActivity.java
+++ app/src/main/java/tan/kai/testtoolbar/MainActivity.java
@@ -1,7 +1,10 @@
 package tan.kai.testtoolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"toolbar左侧图标", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        setSupportActionBar(toolbar);
+
     }
 }
