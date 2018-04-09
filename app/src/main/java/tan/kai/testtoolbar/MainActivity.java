7a479db1ac912a293402806e6488f5a77e0f2b47
luobodidi <1067629251@qq.com>
2018-04-09 11:32:36 +0800

    2018年4月9日11:32:27

--- app/src/main/java/tan/kai/testtoolbar/MainActivity.java
+++ app/src/main/java/tan/kai/testtoolbar/MainActivity.java
@@ -3,6 +3,8 @@ package tan.kai.testtoolbar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,10 +18,67 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"toolbar左侧图标", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "toolbar左侧图标", Toast.LENGTH_SHORT).show();
             }
         });
-//        setSupportActionBar(toolbar);
 
+        //set up menu layout
+//        toolbar.inflateMenu(R.menu.main_menu);
+//        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.menu1:
+//                        show("menu1");
+//                        break;
+//                    case R.id.menu2:
+//                        show("menu2");
+//                        break;
+//                    case R.id.menu3:
+//                        show("menu3");
+//                        break;
+//                    case R.id.menu4:
+//                        show("menu4");
+//                        break;
+//                }
+//                return false;
+//            }
+//        });
+
+        //如果要使用onCreateOptionsMenu，就得把toolbar赋给actionbar
+        setSupportActionBar(toolbar);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu1:
+                show("menu1");
+                break;
+            case R.id.menu2:
+                show("menu2");
+                break;
+            case R.id.menu3:
+                show("menu3");
+                break;
+            case R.id.menu4:
+                show("menu4");
+                break;
+
+        }
+        return true;
+    }
+
+    private void show(String text) {
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+    }
+
 }
